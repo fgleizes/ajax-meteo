16ffce699ed720615a1a4c984617407c9c4a1827
Flo Popg13 <florentgleizes@hotmail.com>
2019-10-07 12:32:13 +0200

    js final ajax-meteo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const weatherIcons = {
     "Clouds": "wi wi-day-cloudy",
     "Clear": "wi wi-day-sunny",
     "Snow": "wi wi-day-snow",
-    "mist": "wi wi-day-fog",
+    "Mist": "wi wi-day-fog",
     "Drizzle": "wi wi-day-sleet",
 }
 
@@ -11,45 +11,32 @@ function capitalize(str){
     return str[0].toUpperCase() + str.slice(1);
 }
 
-async function main(){
-    // fetch('https://api.ipify.org?format=json')
-    //     .then(resultat => resultat.json())
-    //     .then(json => {
-    //         const ip = json.ip;
+async function main(withIp = true){
+    let ville;
 
-    //         fetch(`http://api.ipstack.com/${ip}?access_key=13d6aa2601f46509be09d76eb59a55ce`)
-    //             .then(resultat => resultat.json())
-    //             .then(json => {
-    //                 const ville = json.city;
-
-    //                 fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&APPID=8e602b9ea28ed4f9f8fc97a5f6d1105c&lang=fr&units=metric`)
-    //                 // fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&APPID=b7f13582854c2cf55297dfdbd7b8a665&lang=fr&units=metric`)
-    //                     .then(resultat => resultat.json())
-    //                     .then(json => {
-    //                         // Afficher les informations
-    //                         console.log(json);
-    //                     })
-    //             })
-    //     })
-
-    // On récupère l'adresse IP de l'utilisateur grâce à l'API ipfy.org,
-    const ip = await fetch('https://api.ipify.org?format=json')
-        .then(resultat => resultat.json())
-        .then(json => json.ip)
-
-    // pour retrouver la ville de connexion grâce à l'API ipsatck.com,
-    const ville = await fetch(`http://api.ipstack.com/${ip}?access_key=13d6aa2601f46509be09d76eb59a55ce`)
-        .then(resultat => resultat.json())
-        .then(json => json.city)
+    if (withIp) {
+        // On récupère l'adresse IP de l'utilisateur grâce à l'API ipify.org,
+        const ip = await fetch('https://api.ipify.org?format=json')
+            .then(resultat => resultat.json())
+            .then(json => json.ip)
+    
+        // pour retrouver sa ville de connexion grâce à l'API ipsatck.com,
+        ville = await fetch(`http://api.ipstack.com/${ip}?access_key=13d6aa2601f46509be09d76eb59a55ce`)
+            .then(resultat => resultat.json())
+            .then(json => json.city)
+    }else{
+        ville = document.querySelector('#ville').textContent;
+    }
 
     //et récupérer les informations météos grâce à l'API openweathermap.org !
-    // const meteo = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&APPID=8e602b9ea28ed4f9f8fc97a5f6d1105c&lang=fr&units=metric`)
     const meteo = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&APPID=b7f13582854c2cf55297dfdbd7b8a665&lang=fr&units=metric`)
         .then(resultat => resultat.json())
         .then(json => json)
-
+    
     displayWeatherInfos(meteo)
 }
+
+const aujourdhui = new Date();
 
 function displayWeatherInfos(data){
     const name = data.name;
@@ -60,7 +47,25 @@ function displayWeatherInfos(data){
     document.querySelector('#ville').textContent = name;
     document.querySelector('#temperature').textContent = Math.round(temperature);
     document.querySelector('#conditions').textContent = capitalize(description);
+    document.querySelector('#date-du-jour').textContent = "le " + aujourdhui.getDate() + "/" + (aujourdhui.getMonth() + 1) + "/" + aujourdhui.getFullYear() 
+        + " à " + aujourdhui.getHours() + "h" + aujourdhui.getMinutes();
     document.querySelector('i.wi').className = weatherIcons[conditions];
+
+    document.body.className = conditions.toLowerCase();
 }
+
+const ville = document.querySelector('#ville');
+
+ville.addEventListener('click', () => {
+    ville.contentEditable = true;
+})
+
+ville.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        ville.contentEditable = false;
+        main(false);
+    }
+})
 
 main()
